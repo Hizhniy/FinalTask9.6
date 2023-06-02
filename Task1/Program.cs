4f9730cb6da8579b81171da3906f9d1ec12b8e34
Hizhniy <hizhniy@gmail.com>
2023-06-02 16:26:42 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ProException
+{
+    public class ExceptionNon50 : Exception
+    {
+        public ExceptionNon50() { }
+        public ExceptionNon50(string message)
+        {
+            Console.WriteLine(message == "" ? "Не нужно было вводить 50!.." : message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = { new ExceptionNon50(), new DivideByZeroException(), new OverflowException(), new FormatException(), new FileNotFoundException() };
+
+            Console.Write("Введите число от 1 до 100, но не 50: ");
+            string s = string.Empty;
+            s = Console.ReadLine();
+            try
+            {
+                int number = 0;
+                number = Convert.ToInt32(s);
+                if (number == 50) { throw new ExceptionNon50(""); }
+                if (number == 0) { throw exceptions[1]; }
+                int result = 240 / number;
+
+                Console.Write("Угадайте путь к файлу: ");
+                string filePath = string.Empty;
+                filePath = Console.ReadLine();
+                if (!File.Exists(filePath)) throw exceptions[4];
+                else Console.WriteLine("Файл нашелся!");
+            }
+            catch (Exception e) when (e is ExceptionNon50)
+            {
+            }
+            catch (Exception e) when (e is DivideByZeroException)
+            {
+                Console.WriteLine(string.Concat(e.Message, @"\\\ Мы же делим тут, а вы со своим нулем!"));
+            }
+            catch (Exception e) when (e is OverflowException)
+            {
+                Console.WriteLine(string.Concat(e.Message, @"\\\ Ой, не влезло"));
+            }
+            catch (Exception e) when (e is FormatException)
+            {
+                Console.WriteLine(string.Concat(e.Message, @"\\\ Форматом поломал систему"));
+            }
+            catch (Exception e) when (e is FileNotFoundException)
+            {
+                Console.WriteLine(string.Concat(e.Message, @"\\\ А файла то и нет"));
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿class Namespace
+{
+    class NumberReader
+    {
+        public delegate void NumberEnteredDelegate(int number);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.Write("Введите тип сортировки - 1 (А-Я) / 2 (Я-А): ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number != 1 && number != 2) throw new FormatException();
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
+        }
+    }
+
+    class Program
+    {
+        static string[] LastNames = { "Юдин", "Иванов", "Артамонова", "Семенов", "Петрова" };
+
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEnteredEvent += ShowArray;
+
+            try
+            {
+                numberReader.Read();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено некорректное значение...");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Введено неприлично большое значение...");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        static void ShowArray(int number)
+        {
+            Array.Sort(LastNames);
+            if (number == 2) Array.Reverse(LastNames);
+
+            Console.WriteLine("Мы отсортировали список по вашему требованию:");
+            foreach (string name in LastNames) Console.WriteLine(name);
+        }
+    }
+}
